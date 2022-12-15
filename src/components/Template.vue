9a787f676acd18b081b7bb8faf84dd6706722dde
Daniil <dgandapas1@gmail.ru>
2022-12-15 22:02:44 +0200

    Ask master how link bootstrap

---
+++ src/components/Template.vue
@@ -0,0 +1,15 @@
+<template><!-- только один элемент див(корневой) /-->
+  <div>
+
+  </div>
+</template>
+<script>
+  export default {
+
+  }
+
+</script>
+<!-- scoped  значит стили только для файла template/-->
+<style lang="scss" scoped>
+
+</style>
